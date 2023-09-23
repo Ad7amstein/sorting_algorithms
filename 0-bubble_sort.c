862327af78938796bacef0af46d728ac372fc3b2
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-23 04:38:54 -0700

    fixed the swapping in swap func

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] < array[i - 1])
 			{
-				_swap(array + i, array + i - 1, size);
+				_swap(array + i, array + i - 1);
 				print_array(array, size);
 				swapped = 1;
 			}
